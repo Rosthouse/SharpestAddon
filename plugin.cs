8db6e9ab15ddd28b3b0d73153adc1c72d2a1aabb
Patrick Joo <Patrick.Joos@ost.ch>
2021-07-27 20:45:52 +0200

    Adding [Tool] property to plugin class

--- plugin.cs
+++ plugin.cs
@@ -5,23 +5,24 @@ using System;
 namespace rosthouse.sharpest.addons
 {
 
-    public class plugin : EditorPlugin
-    {
-        // Declare member variables here. Examples:
-        // private int a = 2;
-        // private string b = "text";
+	[Tool]
+	public class plugin : EditorPlugin
+	{
+		// Declare member variables here. Examples:
+		// private int a = 2;
+		// private string b = "text";
 
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready()
-        {
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
 
-        }
+		}
 
-        //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-        //  public override void _Process(float delta)
-        //  {
-        //      
-        //  }
-    }
+		//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+		//  public override void _Process(float delta)
+		//  {
+		//      
+		//  }
+	}
 
 }
