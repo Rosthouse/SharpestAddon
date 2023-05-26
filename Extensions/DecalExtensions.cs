cccc0b782862aebd345c50ec791644dddb823abf
Patrick Joos <Patrick.Joos@ost.ch>
2023-05-26 10:30:28 +0200

    Adding WindowManager to AutoLoads

---
+++ Extensions/DecalExtensions.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace rosthouse.sharpest.addons
+{
+  public static class DecalExtensions
+  {
+    public static Color GetColorAtPosition(this Decal d)
+    {
+      return Colors.White;
+    }
+  }
+}
