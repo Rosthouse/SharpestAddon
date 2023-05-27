69430e677142c73c5226ff9c97f3eecba95009fa
rosthouse <rosthouse@gmail.com>
2023-05-27 19:10:06 +0200

    Adding MathUtils

---
+++ Utils/MathUtils.cs
@@ -0,0 +1,15 @@
+
+
+using System;
+using System.Collections;
+
+namespace rosthouse.sharpest.addons
+{
+	public static class MathUtils
+	{
+		public static bool Between<T>(T min, T max, T t) where T : IComparable
+		{
+			return t.CompareTo(min) >= 0 && t.CompareTo(max) < 0;
+		}
+	}
+}
