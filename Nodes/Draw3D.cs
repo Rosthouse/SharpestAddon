7765343f71990ba18250903f681e716140ae0b28
rosthouse <rosthouse@gmail.com>
2023-05-27 19:10:07 +0200

    Adding simple drawing functions

---
+++ Nodes/Draw3D.cs
@@ -0,0 +1,84 @@
+using Godot;
+using System;
+
+/// <summary>
+/// This class contains some simple functions to draw primitives to the screen
+/// </summary>
+public partial class Draw3D : Node
+{
+  public static Draw3D Instance => _instance;
+  private static readonly Color defaultColor;
+  private static Draw3D _instance;
+
+  static Draw3D()
+  {
+    defaultColor = Colors.WhiteSmoke;
+  }
+
+  public override void _EnterTree()
+  {
+    base._EnterTree();
+    if (_instance != null)
+    {
+      this.QueueFree();
+      return;
+    }
+    _instance = this;
+  }
+
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready()
+  {
+    RenderingServer.FramePostDraw += this.Clear;
+  }
+
+  public MeshInstance3D Line(Vector3 start, Vector3 end, Color color = new Color())
+  {
+    var mi = new MeshInstance3D();
+    var sm = new ImmediateMesh();
+    var mat = new OrmMaterial3D();
+    mat.VertexColorUseAsAlbedo = true;
+
+    mi.Mesh = sm;
+    mi.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
+
+    sm.SurfaceBegin(Mesh.PrimitiveType.Lines);
+    sm.SurfaceSetColor(color);
+    sm.SurfaceAddVertex(start);
+    sm.SurfaceAddVertex(end);
+    sm.SurfaceEnd();
+
+    sm.SurfaceSetMaterial(0, mat);
+    this.AddChild(mi);
+    return mi;
+  }
+
+  public MeshInstance3D Point(Vector3 pos, float radius = 0.05f, Color color = new Color())
+  {
+    var mi = new MeshInstance3D();
+    var sm = new SphereMesh();
+    var mat = new OrmMaterial3D();
+
+    mi.Mesh = sm;
+    mi.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
+    mi.Position = pos;
+
+    sm.Radius = radius;
+    sm.Height = radius * 2;
+    sm.Material = mat;
+
+    mat.ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded;
+    mat.AlbedoColor = color;
+
+    this.AddChild(mi);
+    return mi;
+  }
+
+  public void Clear()
+  {
+    foreach (var c in this.GetChildren())
+    {
+      c.QueueFree();
+    }
+  }
+}
--- SharpestAddon.cs
+++ SharpestAddon.cs
@@ -5,18 +5,24 @@ using System;
 namespace rosthouse.sharpest.addons
 {
 
-	[Tool]
-	public partial class SharpestAddon : EditorPlugin
-	{
-		
-		public override void _EnterTree(){
+  [Tool]
+  public partial class SharpestAddon : EditorPlugin
+  {
 
-			this.AddCustomType("Quit", "Node", GD.Load<Script>("res://addons/SharpestAddon/Nodes/Quit.cs"), GD.Load<Texture2D>("res://addons/SharpestAddon/Nodes/quit.svg"));
-		}
+    public override void _EnterTree()
+    {
 
-		public override void _ExitTree(){
-			this.RemoveCustomType("Quit");
-		}
-	}
+      this.AddCustomType("Quit", "Node", GD.Load<Script>("res://addons/SharpestAddon/Nodes/Quit.cs"), GD.Load<Texture2D>("res://addons/SharpestAddon/Nodes/quit.svg"));
+      this.AddCustomType("Draw3D", "Node", GD.Load<Script>("res://addons/SharpestAddon/Nodes/Draw3D.cs"), GD.Load<Texture2D>("res://addons/SharpestAddon/Nodes/draw3d.svg"));
+      this.AddAutoloadSingleton("Draw3D", "res://addons/SharpestAddon/Nodes/Draw3D.cs");
+    }
+
+    public override void _ExitTree()
+    {
+      this.RemoveAutoloadSingleton("Draw3D");
+      this.RemoveCustomType("Quit");
+      this.RemoveCustomType("Draw3D");
+    }
+  }
 
 }
