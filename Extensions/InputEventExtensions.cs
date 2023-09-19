0591e58fe29ee46f4404124b75bc14ca6608655e
rosthouse <rosthouse@gmail.com>
2023-09-19 13:17:12 +0200

    Adding Method to check if any action has been acted upon

--- Extensions/InputEventExtensions.cs
+++ Extensions/InputEventExtensions.cs
@@ -16,4 +16,16 @@ public static class InputEventExtensions
     }
     return false;
   }
+
+  public static bool IsAnyAction(params string[] actions)
+  {
+    foreach (var action in actions)
+    {
+      if (Input.IsActionPressed(action) || Input.IsActionJustPressed(action) || Input.IsActionJustReleased(action))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
 }
