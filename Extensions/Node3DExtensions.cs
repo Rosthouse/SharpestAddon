26092f5fbe2237fb7320bbf2a81d3edc15aaf753
rosthouse <rosthouse@gmail.com>
2023-09-19 13:10:22 +0200

    Fixing method RotateAround Adding method LookAtOnAxis

--- Extensions/Node3DExtensions.cs
+++ Extensions/Node3DExtensions.cs
@@ -1,16 +1,25 @@
 
 
+using System.Globalization;
 using Godot;
 
 public static class Node3DExtensions
 {
 
-  public static void RotateAround(this Node3D n, Vector3 point, Vector3 axis, float angle)
+  public static void RotateAround(this Node3D n, Vector3 pivot, Vector3 axis, float angle)
   {
-    var rot = angle + n.Rotation.Y;
-    var tStart = point;
-    n.GlobalTranslate(-tStart);
-    n.Transform = n.Transform.Rotated(axis, -rot);
-    n.GlobalTranslate(tStart);
+    var q = new Quaternion(axis, angle);
+    n.GlobalPosition = q * (n.GlobalPosition - pivot) + pivot;
+    n.GlobalRotation = q * n.GlobalRotation;
+  }
+
+  public static void LookAtOnAxis(this Node3D n, Vector3 target, Vector3 axis)
+  {
+    Vector3 targetPostition = new Vector3(
+      axis.X > 0 ? n.GlobalPosition.X : target.X,
+      axis.Y > 0 ? n.GlobalPosition.Y : target.Y,
+      axis.Z > 0 ? n.GlobalPosition.Z : target.Z
+    );
+    n.LookAt(targetPostition);
   }
 }
