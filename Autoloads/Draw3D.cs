489d7b9915093a09cc452c5f91b69108b31c28c4
Patrick.Joos <Patrick.Joos@ost.ch>
2024-09-22 22:09:14 +0200

    Fixing some minor code formatting

--- Autoloads/Draw3D.cs
+++ Autoloads/Draw3D.cs
@@ -1,97 +1,95 @@
 using Godot;
 using System;
 
-namespace rosthouse.sharpest.addon
+namespace rosthouse.sharpest.addon;
+/// <summary>
+/// This class contains some simple functions to draw primitives to the screen
+/// </summary>
+public partial class Draw3D : Node
 {
-  /// <summary>
-  /// This class contains some simple functions to draw primitives to the screen
-  /// </summary>
-  public partial class Draw3D : Node
+  public static Draw3D Instance => _instance;
+  private static readonly Color defaultColor;
+  private static Draw3D _instance;
+
+  static Draw3D()
   {
-    public static Draw3D Instance => _instance;
-    private static readonly Color defaultColor;
-    private static Draw3D _instance;
+    defaultColor = Colors.WhiteSmoke;
+  }
 
-    static Draw3D()
+  public override void _EnterTree()
+  {
+    base._EnterTree();
+    if (_instance != null)
     {
-      defaultColor = Colors.WhiteSmoke;
+      this.QueueFree();
+      return;
     }
+    _instance = this;
+  }
 
-    public override void _EnterTree()
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready()
+  {
+    RenderingServer.FramePostDraw += this.Clear;
+  }
+
+  public MeshInstance3D Line(Vector3 start, Vector3 end, Color color = new Color())
+  {
+    var mi = new MeshInstance3D();
+    var sm = new ImmediateMesh();
+    var mat = new OrmMaterial3D();
+    mat.VertexColorUseAsAlbedo = true;
+
+    mi.Mesh = sm;
+    mi.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
+
+    sm.SurfaceBegin(Mesh.PrimitiveType.Lines);
+    sm.SurfaceSetColor(color);
+    sm.SurfaceAddVertex(start);
+    sm.SurfaceAddVertex(end);
+    sm.SurfaceEnd();
+
+    sm.SurfaceSetMaterial(0, mat);
+    this.AddChild(mi);
+    return mi;
+  }
+
+  public MeshInstance3D Point(Vector3 pos, float radius = 0.05f, Color color = new Color())
+  {
+    var mi = new MeshInstance3D();
+    var sm = new SphereMesh();
+    var mat = new OrmMaterial3D();
+
+    mi.Mesh = sm;
+    mi.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
+    mi.Position = pos;
+
+    sm.Radius = radius;
+    sm.Height = radius * 2;
+    sm.Material = mat;
+
+    mat.ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded;
+    mat.AlbedoColor = color;
+
+    this.AddChild(mi);
+    return mi;
+  }
+
+  public Label3D Text(Vector3 pos, String text, Color c = new Color())
+  {
+    Label3D l = new Label3D();
+    l.Text = text;
+    l.Billboard = BaseMaterial3D.BillboardModeEnum.Enabled;
+
+    this.AddChild(l);
+    return l;
+  }
+
+  public void Clear()
+  {
+    foreach (var c in this.GetChildren())
     {
-      base._EnterTree();
-      if (_instance != null)
-      {
-        this.QueueFree();
-        return;
-      }
-      _instance = this;
-    }
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-      RenderingServer.FramePostDraw += this.Clear;
-    }
-
-    public MeshInstance3D Line(Vector3 start, Vector3 end, Color color = new Color())
-    {
-      var mi = new MeshInstance3D();
-      var sm = new ImmediateMesh();
-      var mat = new OrmMaterial3D();
-      mat.VertexColorUseAsAlbedo = true;
-
-      mi.Mesh = sm;
-      mi.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
-
-      sm.SurfaceBegin(Mesh.PrimitiveType.Lines);
-      sm.SurfaceSetColor(color);
-      sm.SurfaceAddVertex(start);
-      sm.SurfaceAddVertex(end);
-      sm.SurfaceEnd();
-
-      sm.SurfaceSetMaterial(0, mat);
-      this.AddChild(mi);
-      return mi;
-    }
-
-    public MeshInstance3D Point(Vector3 pos, float radius = 0.05f, Color color = new Color())
-    {
-      var mi = new MeshInstance3D();
-      var sm = new SphereMesh();
-      var mat = new OrmMaterial3D();
-
-      mi.Mesh = sm;
-      mi.CastShadow = GeometryInstance3D.ShadowCastingSetting.Off;
-      mi.Position = pos;
-
-      sm.Radius = radius;
-      sm.Height = radius * 2;
-      sm.Material = mat;
-
-      mat.ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded;
-      mat.AlbedoColor = color;
-
-      this.AddChild(mi);
-      return mi;
-    }
-
-    public Label3D Text(Vector3 pos, String text, Color c = new Color())
-    {
-      Label3D l = new Label3D();
-      l.Text = text;
-      l.Billboard = BaseMaterial3D.BillboardModeEnum.Enabled;
-
-      this.AddChild(l);
-      return l;
-    }
-
-    public void Clear()
-    {
-      foreach (var c in this.GetChildren())
-      {
-        c.QueueFree();
-      }
+      c.QueueFree();
     }
   }
 }
--- Autoloads/WindowManager.cs
+++ Autoloads/WindowManager.cs
@@ -1,78 +1,76 @@
 using Godot;
 using System;
 
-namespace rosthouse.sharpest.addon
+namespace rosthouse.sharpest.addon;
+public partial class WindowManager : Node
 {
-  public partial class WindowManager : Node
+
+  private static WindowManager _instance;
+  public static WindowManager Instance => _instance;
+
+
+  public override void _EnterTree()
   {
-
-    private static WindowManager _instance;
-    public static WindowManager Instance => _instance;
-
-
-    public override void _EnterTree()
+    if (_instance != null)
     {
-      if (_instance != null)
-      {
-        this.QueueFree();
-      }
-      else
-      {
-        _instance = this;
-      }
+      this.QueueFree();
     }
-
-    public void OpenWindow(Control n)
+    else
     {
-      this.OpenWindow(n, GetViewport().GetVisibleRect().Size / 2);
+      _instance = this;
     }
+  }
 
-    public void OpenWindow(Control windowContent, Vector2 position, string title = "")
-    {
-      var lw = GD.Load<PackedScene>("res://addons/SharpestAddon/Nodes/light_window.tscn").Instantiate<LightWindow>();
-      this.AddChild(lw);
-      lw.SetContent(windowContent, true);
-      lw.SetTitle(title);
-      lw.Passthrough = true;
-      lw.RespectContentMinSize = true;
-      lw.Position = position;
-    }
+  public void OpenWindow(Control n)
+  {
+    this.OpenWindow(n, GetViewport().GetVisibleRect().Size / 2);
+  }
 
-    public void OpenWindow(Control windowContent, Vector3 position)
+  public void OpenWindow(Control windowContent, Vector2 position, string title = "")
+  {
+    var lw = GD.Load<PackedScene>("res://addons/SharpestAddon/Nodes/light_window.tscn").Instantiate<LightWindow>();
+    this.AddChild(lw);
+    lw.SetContent(windowContent, true);
+    lw.SetTitle(title);
+    lw.Passthrough = true;
+    lw.RespectContentMinSize = true;
+    lw.Position = position;
+  }
+
+  public void OpenWindow(Control windowContent, Vector3 position)
+  {
+    var screenPos = GetWindow().GetCamera3D().UnprojectPosition(position);
+    this.OpenWindow(windowContent, screenPos);
+  }
+
+  public void OpenWindowTruncated(Control windowContent, Vector3 position)
+  {
+    var screenPos = GetWindow().GetCamera3D().UnprojectPosition(position);
+    if (GetWindow().GetVisibleRect().HasPoint(screenPos))
     {
-      var screenPos = GetWindow().GetCamera3D().UnprojectPosition(position);
       this.OpenWindow(windowContent, screenPos);
     }
-
-    public void OpenWindowTruncated(Control windowContent, Vector3 position)
+    else
     {
-      var screenPos = GetWindow().GetCamera3D().UnprojectPosition(position);
-      if (GetWindow().GetVisibleRect().HasPoint(screenPos))
-      {
-        this.OpenWindow(windowContent, screenPos);
-      }
-      else
-      {
-        this.OpenWindow(windowContent, GetViewport().GetVisibleRect().Size / 2);
-      }
+      this.OpenWindow(windowContent, GetViewport().GetVisibleRect().Size / 2);
     }
+  }
 
-    public void OpenPopup(Popup windowContent, Vector3 position)
-    {
-      var screenPos = GetWindow().GetCamera3D().UnprojectPosition(position);
-      this.OpenPopup(windowContent, screenPos);
-    }
+  public void OpenPopup(Popup windowContent, Vector3 position)
+  {
+    var screenPos = GetWindow().GetCamera3D().UnprojectPosition(position);
+    this.OpenPopup(windowContent, screenPos);
+  }
 
-    public void OpenPopup(Popup popup, Vector2 position)
-    {
+  public void OpenPopup(Popup popup, Vector2 position)
+  {
 
-      popup.FocusExited += () => popup.QueueFree();
-      this.AddChild(popup);
-      popup.Popup(new Rect2I(
-       position.RountToInt(),
-        popup.Size)
-      );
+    popup.FocusExited += () => popup.QueueFree();
+    this.AddChild(popup);
+    popup.Popup(new Rect2I(
+     position.RountToInt(),
+      popup.Size)
+    );
 
-    }
   }
 }
