3370590f9de58dfb5e5fc517e0a26f17fa72f7c5
Rosthouse <rosthouse@gmail.com>
2024-12-12 14:22:45 +0100

    Adding rect conversion to system.numerics.vector4 and bakc

--- Extensions/RectExtensions.cs
+++ Extensions/RectExtensions.cs
@@ -14,5 +14,34 @@ namespace rosthouse.sharpest.addon
 				(float)GD.RandRange(rect.Position.Y, rect.End.Y)
 			);
 		}
+
+		public static System.Numerics.Vector4 ToSystemVector4(this Rect2 rect)
+		{
+			return new System.Numerics.Vector4
+			{
+				X = rect.Position.X,
+				Y = rect.Position.Y,
+				Z = rect.End.X,
+				W = rect.End.Y
+			};
+		}
+
+
+		public static Rect2 ToRect2(this System.Numerics.Vector4 vector)
+		{
+			return new Rect2
+			{
+				Position = new Vector2
+				{
+					X = vector.X,
+					Y = vector.Y
+				},
+				End = new Vector2
+				{
+					X = vector.Z,
+					Y = vector.W
+				}
+			};
+		}
 	}
 }
