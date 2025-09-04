f122e96d3468391b17dc2f89b967c0595ec0e96c
Rosthouse <rosthouse@gmail.com>
2025-09-04 22:01:07 +0200

    Reworing this plugin, to have tests and the like

--- addons/SharpestAddon/Nodes/Quit.cs
+++ addons/SharpestAddon/Nodes/Quit.cs
@@ -5,13 +5,13 @@ namespace rosthouse.sharpest.addon;
 
 public partial class Quit : Node
 {
-    [Export] private string quitAction = "ui_end";
-    public override void _Input(InputEvent @event)
+  [Export] private string quitAction = "ui_end";
+  public override void _Input(InputEvent @event)
+  {
+    if (@event.IsActionPressed(quitAction) && OS.GetName() != "HTML5")
     {
-        if (@event.IsActionPressed(quitAction) && OS.GetName() != "HTML5")
-        {
-            GetTree().Quit();
-        }
-        base._Input(@event);
+      GetTree().Quit();
     }
+    base._Input(@event);
+  }
 }
